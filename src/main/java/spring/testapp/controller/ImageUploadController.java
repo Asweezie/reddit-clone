cfd1faddef82c410bec26c95536dcf36e19ca4e1
brian <brianduong321@gmail.com>
2024-05-03 17:08:16 -0700

    add image gallery

--- src/main/java/spring/testapp/controller/ImageUploadController.java
+++ src/main/java/spring/testapp/controller/ImageUploadController.java
@@ -4,16 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import spring.testapp.service.ImageStorageService;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/api/images")
+@RequestMapping("/images")
 public class ImageUploadController {
     private final ImageStorageService storageService;
 
@@ -21,6 +20,13 @@ public class ImageUploadController {
     public ImageUploadController(ImageStorageService storageService) {
 
         this.storageService = storageService;
+    }
+
+    @GetMapping("/gallery")
+    public String gallery(Model model) {
+        List<String> imageUrls = storageService.listImages("redditcloneimages");
+        model.addAttribute("imageUrls", imageUrls);
+        return "images/gallery";
     }
 
     @PostMapping("/upload")
--- src/main/java/spring/testapp/service/ImageStorageService.java
+++ src/main/java/spring/testapp/service/ImageStorageService.java
@@ -6,7 +6,10 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,8 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ImageStorageService {
@@ -38,6 +43,26 @@ public class ImageStorageService {
         AWSCredentials credentials = new BasicAWSCredentials(this.awsId, this.awsKey);
         this.s3client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(Regions.fromName(region)).build();
+    }
+
+
+    public String fetchImageURL(String bucketName, String fileName) {
+        String fileUrl = "";
+        try {
+            fileUrl = s3client.getUrl(bucketName, fileName).toExternalForm();
+        } catch (AmazonS3Exception e) {
+            e.printStackTrace();
+        }
+        return fileUrl;
+    }
+
+    public List<String> listImages(String bucketName) {
+        ListObjectsV2Result result = s3client.listObjectsV2(bucketName);
+        List<S3ObjectSummary> objects = result.getObjectSummaries();
+
+        return objects.stream()
+                .map(obj -> s3client.getUrl(bucketName, obj.getKey()).toExternalForm())
+                .collect(Collectors.toList());
     }
 
     public String uploadFile(MultipartFile file) {
